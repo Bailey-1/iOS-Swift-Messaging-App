7e63da47fc61c5aba746e4f5007cbcd216a07de2
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2020-07-20 15:43:22 +0100

    Started working on the messaging app.
    Started working on the messaging app.

---
+++ messaging-app/messaging-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  messaging-app
+//
+//  Created by Bailey Search on 20/07/2020.
+//  Copyright © 2020 Bailey Search. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
